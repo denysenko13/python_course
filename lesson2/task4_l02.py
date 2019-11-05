3206a192e903065f49562a1b9984deb4d4ed524c
Vladyslav Denysenko <denysenko@ksolutions.com.ua>
2019-11-05 16:38:37 +0200

    Some fixes

--- lesson2/task4_l02.py
+++ lesson2/task4_l02.py
@@ -1,4 +1,7 @@
 number = int(input("Q: Enter the number\nA: "))
 power = int(input("Q: Enter the power\nA: "))
-print("Type **:", number ** power)
-print("Type pow():", pow(number, power))
+result_1 = number ** power
+result_2 = pow(number, power)
+print("Result:", result_1, "Type **:")
+print("Result:", result_2, "Type pow():")
+print("Is result1 equal for result2?", result_1 == result_2)
--- lesson2/task6_l02.py
+++ lesson2/task6_l02.py
@@ -1,3 +1,3 @@
-number = float(input("Q:Enter the int or float:\nA:"))
-print("hex:", float(hex((number))))
-print("oct", float(oct((number))))
+number = float(input("Q:Enter the int or float:\nA: "))
+print("hex:", hex(round(number)))
+print("oct", oct(round(number)))
